ac12b25239b3a4005ddc8a51d139e41e5cf2bbaa
PedroSawczuk <pedrosawczuk53@gmail.com>
2024-12-10 14:46:50 -0400

    Update AdminPage

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Meta
 
-# Register your models here.
+class MetaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'tempo', 'email', 'criado_em')
+    search_fields = ('nome', 'email')
+    list_filter = ('tempo',)
+    list_editable = ('tempo',)
+    ordering = ('-criado_em',)
+
+admin.site.register(Meta, MetaAdmin)
